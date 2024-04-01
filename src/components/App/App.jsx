7c5249a6bcdfc97f7c79e2d5d479ab3f40d17f55
Lilia Levchenko <liliya.benatti@gmail.com>
2024-04-01 15:59:30 +0200

    api request added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,29 +11,39 @@ import { getPhotos } from "../../APIService/APIService";
 export default function App() {
   const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
-  const [photos, setPhotos] = useState([]);
+  const [images, setImages] = useState([]);
+  const [totalResults, setTotalResults] = useState(0);
   const [query, setQuery] = useState("");
 
   useEffect(() => {
     if (!query) return;
-    const fetchData = async () => {
-      try {
-        const response = await getPhotos("your_query", 1);
-      } catch (error) {
-        setError(error.message);
-      }
-    };
-
-    fetchData();
+    getPhotos(query, page)
+      .then(({ results, total_pages }) => {
+        setImages((prev) => [...prev, ...results]);
+        setTotalResults(total_pages);
+      })
+      .catch((error) => setError(error.message));
   }, [query, page]);
 
+  function handleSubmit(query) {
+    setImages([]);
+    setPage(1);
+    setError(null);
+    setTotalResults(0);
+    setQuery(query);
+  }
+  function handleLoadMore() {
+    setPage(page + 1);
+  }
   return (
     <>
-      <LoadMoreBtn />
-      <SearchBar />
-      <ImageGallery />
+      <SearchBar onSubmit={handleSubmit} />
+      <ImageGallery images={images} />
+      {error && <ErrorMessage textAlign="center">{error}🔎</ErrorMessage>}
+      {totalResults > 0 && images.length < totalResults && (
+        <LoadMoreBtn onClick={handleLoadMore}>Load more</LoadMoreBtn>
+      )}
       <Loader />
-      <ErrorMessage />
       <ImageModal />
     </>
   );
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,33 @@
 import css from "./SearchBar.module.css";
+import { useState } from "react";
 
-export const SearchBar = () => {
+export const SearchBar = ({ onSubmit }) => {
+  const [query, setQuery] = useState("");
+
+  function saveInput(event) {
+    const inputValue = event.target.value;
+    setQuery(inputValue);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmit(query);
+    setQuery("");
+  }
   return (
     <div>
-      <h2>SearchBar</h2>
+      <header>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={saveInput}
+            value={query}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </header>
     </div>
   );
 };
